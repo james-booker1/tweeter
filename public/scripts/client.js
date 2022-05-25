aef4e0366e098aedd33ef302e1772d57fd3a36a5
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-05-25 09:47:57 -0700

    adding auto loading tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,28 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function () {
+  const tweetData = {
+    // user: {
+    //   name: "Newton",
+    //   avatars: "https://i.imgur.com/73hZDYK.png",
+    //   handle: "@SirIsaac",
+    // },
+    // content: {
+    //   text: "If I have seen further it is by standing on the shoulders of giants",
+    // },
+    // created_at: 1461116232227,
+  };
 
+  const createTweetElement = () => {
+    const $tweet = $(`<article class="tweet">Hello world</article>`);
+    return $tweet;
+  };
+
+  const $tweet = createTweetElement(tweetData);
+
+  console.log($tweet);
+  $(".tweets").append($tweet);
+
+  const renderTweets = (tweetData) => {};
+});
